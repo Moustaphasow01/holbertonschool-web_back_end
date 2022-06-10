39422fbc81e6ebc02350e3f18d21fc6cc12cad0a
manolobkno08 <manologm10@hotmail.com>
2022-06-10 15:19:10 -0500

    ADD: Taskt 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,6 +6,7 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """Returns the sum of list as a float"""
-    total: float = 0
-    r = [total := total + x for x in input_list]
-    return r[2]
+    return sum(input_list)
+    # total: float = 0
+    # r = [total := total + x for x in input_list]
+    # return r[2]
