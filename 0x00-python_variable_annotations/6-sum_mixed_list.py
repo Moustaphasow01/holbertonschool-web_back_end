1a8e1719a1dcd1da820a8c708d7ef5fa1cb60b85
manolobkno08 <manologm10@hotmail.com>
2022-06-10 15:34:51 -0500

    ADD: Taskt 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Complex types - mixed list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of list (int, floats) as a float"""
+    return sum(mxd_lst)
