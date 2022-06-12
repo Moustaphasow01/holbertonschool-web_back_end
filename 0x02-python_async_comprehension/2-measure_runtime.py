cfea7247fb484dd0e0f1349da1d53a49badcb414
manolobkno08 <manologm10@hotmail.com>
2022-06-12 12:29:22 -0500

    ADD: Task 1

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Import async_comprehension from the previous file and write a
-    measure_runtime coroutine that will execute async_comprehension"""
+""" 2. Run time for four parallel comprehensions """
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
