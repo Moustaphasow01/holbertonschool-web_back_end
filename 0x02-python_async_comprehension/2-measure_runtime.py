99ad5c8dd9ef8503c27ee0aec6fdcabeeb8e0368
manolobkno08 <manologm10@hotmail.com>
2022-06-12 12:26:06 -0500

    ADD: Task 1

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Import async_comprehension from the previous file and write a
-    measure_runtime coroutine that will execute async_comprehension four times """
+    measure_runtime coroutine that will execute async_comprehension"""
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
